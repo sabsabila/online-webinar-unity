76d51306121ca029d51b8b7242a029da4b69b4aa
sabilajml <sabilajml@gmail.com>
2022-11-23 14:07:22 +0700

    init commit

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> panelMenus;
+
+    public void ShowSelectedMenu(int index)
+    {
+        for(int i = 0; i < panelMenus.Count; i++)
+        {
+            if (i == index)
+                panelMenus[i].SetActive(true);
+            else
+                panelMenus[i].SetActive(false);
+        }
+    }
+}
